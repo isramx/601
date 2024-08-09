c7b549418d053892c08965910542806af694dcb0
isramx <adolfopicazo99@gmail.com>
2024-08-09 21:23:04 +0000

    Descripción de los cambios realizados

---
+++ script.js
@@ -0,0 +1,5 @@
+// Obtén una referencia al botón usando su ID
+document.getElementById('BTNQUEJA').addEventListener('click', function() {
+    // Redirecciona a la página deseada
+    window.location.href = 'https://www.google.com';
+});
